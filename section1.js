425d38094ce538794db375da51a0a65cbdaef8be
NandiniExathought <nandini.gnanesh@exathought.com>
2022-08-03 13:05:59 +0530

    section 1 added

---
+++ section1.js
@@ -0,0 +1,32 @@
+//----------------------------------------------------------------------------------------------
+//                                    Introduction to the course
+//----------------------------------------------------------------------------------------------
+
+
+//----------------------------------------------------------------------------------------------
+//                                    what is flutter
+//----------------------------------------------------------------------------------------------
+/*
+before flutter, we build ios apps using one language, and android apps using another language.
+To overcome from these problems, we need to use flutter now
+*/
+
+//----------------------------------------------------------------------------------------------
+//                                    why flutter
+//----------------------------------------------------------------------------------------------
+
+//one codebase to maintain all, one place to debug, one place to update -> this is the biggest advantage of flutter.
+/*
+1. Flutter allows you to use a very simple and flexible layout system to build beautiful user interface for whatever project you end up embarking on.  
+*/
+
+//----------------------------------------------------------------------------------------------
+//                                    The Anatomy of a Flutter App
+//----------------------------------------------------------------------------------------------
+
+/*
+Tips :
+1. first watch 10 min video, then start coding
+2. keybr.com -> helps in improving typing skills.
+3. for taking notes, I can use Cornell note.
+*/
