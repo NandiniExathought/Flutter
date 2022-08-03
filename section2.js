1907163db453ae305ceaf24d4cb0ad47ca8fc8d2
NandiniExathought <nandini.gnanesh@exathought.com>
2022-08-03 17:45:40 +0530

    section3 - creating a new flutter project from scratch added

--- section2.js
+++ section2.js
@@ -5,4 +5,5 @@
 1. need to have a pc
 2. android studio as a IDE
 3. Android emulator or physical device.
+note: ctrl+shift+G => short hand property for creating new flutter project
 */
---
+++ section3.js
@@ -0,0 +1,21 @@
+//----------------------------------------------------------------------------------------
+//          creating a new flutter project from scratch
+//----------------------------------------------------------------------------------------
+
+/*
+using widget tree we have built an hello-world app.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello-world'),
+      ),
+    ),
+  );
+}
+
+
+*/
